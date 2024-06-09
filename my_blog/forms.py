729a943bb27c8a2355ff7424e62b071c61299cfe
Pi Rson <duwunaung@gmail.com>
2024-06-09 14:23:23 +0630

    Session2 (Day2)

---
+++ my_blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Contact, Comment
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ("name", "email", "message", )
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("name", "email", "message", )
